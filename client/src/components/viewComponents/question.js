128165ac5b7ec52bac87c340fe06740edf5467bb
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-03-31 18:22:06 -0400

    question box created

---
+++ client/src/components/viewComponents/question.js
@@ -0,0 +1,18 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+import Flag from "../../assets/english_flag.png";
+
+function Question() {
+    return (
+        <div className="questions">
+            <img src={Flag} width="50px" height="50px"/>
+            <p>Universities and colleges are now offering qualifications through distance learning from the internet rather than teachers in the classroom. Do you think the advantages of this development outweigh the disadvantages?</p>
+        </div>
+    );
+}
+    
+export default Question;
+=======
+>>>>>>> 05a8e7bbb1aca0d1d6c9f05fded123e493af4fc6
+>>>>>>> Stashed changes
