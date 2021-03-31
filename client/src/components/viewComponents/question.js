9358da14d1fb381a62993a44fb9f807f20515eed
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-03-31 18:32:22 -0400

    removed stashed change text

--- client/src/components/viewComponents/question.js
+++ client/src/components/viewComponents/question.js
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
 import Flag from "../../assets/english_flag.png";
 
 function Question() {
@@ -13,6 +10,4 @@ function Question() {
 }
     
 export default Question;
-=======
->>>>>>> 05a8e7bbb1aca0d1d6c9f05fded123e493af4fc6
->>>>>>> Stashed changes
+
