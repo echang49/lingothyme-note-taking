3c827b9423a897f1092e6028ffc4a73da3f930c8
Edward <46017382+echang49@users.noreply.github.com>
2021-04-25 22:21:41 -0700

    Redesign and Navbar added

--- client/src/components/editorView.js
+++ client/src/components/editorView.js
@@ -3,6 +3,10 @@ import { Redirect, useLocation } from "react-router-dom";
 import axios from "axios";
 import Question from "./viewComponents/question";
 
+import {ReactComponent as Logo} from "../assets/logo-white.svg";
+import {ReactComponent as Brace} from "../assets/right-brace.svg";
+import {ReactComponent as Note} from "../assets/note-icon.svg";
+
 
 function EditorView() {
     const location = useLocation().search;
@@ -25,9 +29,28 @@ function EditorView() {
         <div>
             {
                 bool ?
-                    <div className="testing">
-                        <p>EDITOR VIEW</p>
-                        <Question />
+                    <div className="userView">
+                        <nav>
+                            <span className="nav-start">
+                                <Logo />
+                            </span>
+                            <span className="nav-roomphase">
+                                <p>Brainstorming Room</p>
+                            </span>
+                            <span className="nav-center">
+                                <Note className="note-icon" />
+                            </span>
+                            <span className="nav-end">
+                                <button>
+                                    <p>Leave </p>
+                                    <Brace />
+                                </button>
+                            </span>
+                        </nav>
+                        {/* <div className="testing">
+                            <Question />
+                        </div> */}
+                        {/**Have 4 different components. Meeting has not started, brainstorming, collaborative writing, meeting has finished */}
                     </div>
                 :
                     <Redirect to="/" />
