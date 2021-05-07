550c83e275889bdf7782bd7f1f924ee63a590ca0
Edward <46017382+echang49@users.noreply.github.com>
2021-05-06 21:11:11 -0400

    Proper Merge Pt2

--- client/src/components/editorView.js
+++ client/src/components/editorView.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { Redirect, useLocation } from "react-router-dom"; 
+import { useEffect, useState, useRef } from "react";
+import { Redirect, useLocation, Link } from "react-router-dom"; 
+import io from "socket.io-client";
 import axios from "axios";
 import Question from "./viewComponents/question";
 import User from "./viewComponents/users";
-import { Link } from "react-router-dom";
 
+import ColorLogo from "../assets/main-logo.png";
 import {ReactComponent as Logo} from "../assets/logo-white.svg";
 import {ReactComponent as Brace} from "../assets/right-brace.svg";
 import {ReactComponent as Note} from "../assets/note-icon.svg";
@@ -20,16 +21,46 @@ import User8 from "../assets/users/Image8.webp";
 import Brainstorm from "./viewComponents/brainstormResponse";
 
 function EditorView() {
+    const nameInput = useRef(null);
     const location = useLocation().search;
     const [bool, setBool] = useState(true);
-    const [ url, setURL ] = useState("/");
+    const [nameState, setNameState] = useState(true);
+    const [phase, setPhase] = useState(1);
+    const [userList, setUserList] = useState([]);
+
+    const socket = io("http://127.0.0.1:5000", {
+        withCredentials: true
+    });
+
+    socket.on('connection', () => {
+        console.log("connection")
+    });
+
+    socket.on('user-connected', (data) => {
+        //data is user name and id
+        console.log(data);
+    })
+
+    socket.on('phase_change', (data) =>  {
+        setPhase(data);
+    });
 
     useEffect(() => {
-        console.log(location);
         axios.post('/api/auth/verifyUser', {location})
         .then((res) => {
-            if(!res.data) {
+            if(!res.data[0]) {
                 setBool(false);
+            }
+            setPhase(res.data[1]);
+            setUserList(res.data[2]);
+            const localStorageName = JSON.parse(localStorage.getItem('name'));
+            const currentTime = Date.now();
+            //see if name exists or is expired
+            if(localStorageName !== null) {
+                if(new Date(localStorageName[1]).getTime() > currentTime) {
+                    setNameState(false);
+                    socket.emit('new-user', location, localStorageName[0]);
+                }
             }
         })
         .catch((err) => {
@@ -37,74 +68,115 @@ function EditorView() {
         });
     }, [location]);
 
-    return (
-        <div>
-            {
-                bool ?
-                    <div className="userView">
-                        <nav>
-                            <span className="nav-start">
-                                <Logo />
-                            </span>
-                            <span className="nav-roomphase">
-                                <p>Brainstorming Room</p>
-                            </span>
-                            <span className="nav-center">
-                                <Note className="note-icon" onClick={() => handleNoteClick()}/>
-                            </span>
-                            <span className="nav-end">
-                                <Link to="/" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                                    <button onClick={() => handleLeaveClick()}>
-                                        <p>Leave </p>
-                                        <Brace />
-                                    </button>
-                                </Link>      
-                            </span>
-                        </nav>
-                        <div className="body">
-                            <div className="canvas">
-                                <div className="testing">
-                                    <Question />
-                                </div>
-                            </div>
-                            <div className="userList">
-                                <div className="userList-header">
-                                    <p>Users</p>
-                                </div>
-                                <div className="userList-body">
-                                    <User picture={User1} name="Edward" />
-                                    <User picture={User2} name="Nanjiong" />
-                                    <User picture={User3} name="Iris" />
-                                    <User picture={User4} name="Janet" />
-                                    <User picture={User5} name="Vimal" />
-                                    <User picture={User6} name="Adrian" />
-                                    <User picture={User7} name="Shubham" />
-                                    <User picture={User8} name="Veronique" />
-                                </div>
-                            </div>
-                        </div>
-                        {/**Have 4 different components. Meeting has not started, brainstorming, collaborative writing, meeting has finished */}
-                    </div>
-                :
-                    <Redirect to="/" />
-            }
-         </div>
-    );
-
-    function handleNoteClick () {
-        console.log("rendering brainstorm component");
-        return (
-            <div>
-                <Brainstorm />
-            </div>
-        );   
+    function setName(name) {
+        const today = new Date();
+        const tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        localStorage.setItem('name', JSON.stringify([name, tomorrow]));
+        setNameState(false);
+        socket.emit('new-user', location, name);
     }
 
-    function handleLeaveClick () { 
-        console.log("signing out...");
-        // sign out user
+    function handleNoteClick () {
+        //useRef. create a Brainstorming component under the testing area  
+    }
 
+    if(nameState === true) {
+        return(
+            <div>
+                {
+                    bool ?
+                        <div className="userView">
+                            <div className="nameState center">
+                                <img src={ColorLogo} alt="LingoThyme logo" height="250px"/>
+                                <div className="input">
+                                    <label>Please Enter Your Name:</label>
+                                    <input type="text" ref={nameInput} />
+                                    <div className="buttons">
+                                        <button className="primary-button" onClick={() => setName(nameInput.current.value)}>Continue</button>
+                                    </div>
+                                    </div>
+                            </div>
+                        </div>
+                    :
+                        <Redirect to="/" />
+                }
+            </div>
+        );
+    }
+    else {
+        switch(phase) {
+            case 1:
+                return(
+                    <div>
+                        {
+                            bool ?
+                                <div className="userView">
+                                    <div className="phase1 center">
+                                        <p className="title">The admin did not start the session yet.<br />Please hold tight while they prepare the session.</p>
+                                    </div>
+                                </div>
+                            :
+                            <Redirect to="/" />
+                        }
+                    </div>
+                );
+            case 2:
+                return(
+                    <div>
+                        {
+                            bool ?
+                                <div className="userView">
+                                    <nav>
+                                        <span className="nav-start">
+                                            <Logo />
+                                        </span>
+                                        <span className="nav-roomphase">
+                                            <p>Brainstorming Room</p>
+                                        </span>
+                                        <span className="nav-center">
+                                            <Note className="note-icon" onClick={() => handleNoteClick()} />
+                                        </span>
+                                        <span className="nav-end">
+                                            <Link to="/" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                                <button>
+                                                    <p>Leave </p>
+                                                    <Brace />
+                                                </button>
+                                            </Link>
+                                        </span>
+                                    </nav>
+                                    <div className="body">
+                                        <div className="canvas">
+                                            <div className="testing">
+                                                <Question />
+                                                <Brainstorm />
+                                            </div>
+                                        </div>
+                                        <div className="userList">
+                                            <div className="userList-header">
+                                                <p>Users</p>
+                                            </div>
+                                            <div className="userList-body">
+                                                <User picture={User1} name="Edward" />
+                                                <User picture={User2} name="Nanjiong" />
+                                                <User picture={User3} name="Iris" />
+                                                <User picture={User4} name="Janet" />
+                                                <User picture={User5} name="Vimal" />
+                                                <User picture={User6} name="Adrian" />
+                                                <User picture={User7} name="Shubham" />
+                                                <User picture={User8} name="Veronique" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            :
+                                <Redirect to="/" />
+                        }
+                    </div>
+                );
         }
+    }
 }
     
 export default EditorView;
