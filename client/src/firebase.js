80036cdc225faa7b817036af3e9ca52035fe4525
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-06-12 04:52:52 -0400

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({ // store 
-    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain:REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId:REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket:REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messageSenderId: REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: REACT_APP_FIREBASE_APP_ID,
-    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messageSenderId:    process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId:  process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId:  process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export const auth = app.auth();
