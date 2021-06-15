d849f6fc3c999a8ef868d269a71d8e1c42f73038
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-06-15 03:49:39 -0400

    Fixed navbar link highlighting

--- client/src/components/viewComponents/navbar.js
+++ client/src/components/viewComponents/navbar.js
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom";
+import { Redirect, Link, useLocation } from "react-router-dom";
 import { useState } from "react";
-
 import ColorLogo from "../../assets/main-logo.png";
 import {ReactComponent as Search} from "../../assets/search-icon.svg"; // edit svg properties, change to camel case 
 import {ReactComponent as Notification} from "../../assets/notification-icon.svg"; // edit svg properties, change to camel case 
@@ -8,13 +7,18 @@ import firebase from "../../firebase.js";
 
 function Navbar(props) {
     const [loggedIn, setLoggedIn] = useState(false);
+    const location = useLocation().pathname; // current path (e.g. /mainHall)
+    const [user, setUser] = useState({ loggedIn: false });
+    const auth = firebase.auth();
     
 
     async function handleLogout() { // logout user
-        setLoggedIn(false);
+        setUser({ loggedIn: false });
+        return auth.signOut();
     }
 
     async function handleSearch(){
+        console.log("location: " + location);
 
     }
     
@@ -22,47 +26,163 @@ function Navbar(props) {
 
     }
     
-    return (
-        <div>
-            <nav>
-                <span className="nav-start">
-                    <img src={ColorLogo} alt="LingoThyme logo" height="50px"/>
-                </span>
-                <span></span>
-                <span className="nav-center">
-                    <p><div className="current-tab"> Hall</div></p>
-                    <div className="break"></div>
-                    <Link to="/schedule" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                            <p><div className="other-tab"> Schedule</div></p>
-                    </Link>
-                    <div className="break"></div>
-                    <Link to="/academy" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                            <p><div className="other-tab"> Academy</div></p>
-                    </Link>
-                    <div className="break"></div>
-                    <Link to="/profile" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                            <p><div className="other-tab"> Profile</div></p>
-                    </Link>
-                </span>
+    switch(location) { // change current tab highlighting depending on current page user is on
+        case '/mainHall': 
+            return (
+                <div>
+                    <nav>
+                        <span className="nav-start">
+                            <img src={ColorLogo} alt="LingoThyme logo" height="50px"/>
+                        </span>
+                        <span></span>
+                        <span className="nav-center">
+                            <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                <p><div className="current-tab"> Hall</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/schedule" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Schedule</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/academy" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Academy</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/profile" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Profile</div></p>
+                            </Link>
+                        </span>
+        
+                        <span className="nav-end">
+                            <button onClick={() => handleSearch()}>
+                                <Search />
+                            </button>
+        
+                            <button onClick={() => showNotifications()}>
+                                <Notification />
+                            </button>
 
-                <span className="nav-end">
-                    <button onClick={() => handleSearch()}>
-                        <Search />
-                    </button>
-
-                    <button onClick={() => showNotifications()}>
-                        <Notification />
-                    </button>
-
-                    <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                        <button onClick={() => handleLogout()}>
-                            <p>Logout</p>
-                        </button>
-                    </Link>
-                </span>
-            </nav>
-        </div>
-    );
+                            <div className="center"><p>UserName</p></div>
+                        </span>
+                    </nav>
+                </div>
+            );
+        case '/schedule': 
+            return (
+                <div>
+                    <nav>
+                        <span className="nav-start">
+                            <img src={ColorLogo} alt="LingoThyme logo" height="50px"/>
+                        </span>
+                        <span></span>
+                        <span className="nav-center">
+                            <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                <p><div className="other-tab"> Hall</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/schedule" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="current-tab"> Schedule</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/academy" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Academy</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/profile" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Profile</div></p>
+                            </Link>
+                        </span>
+        
+                        <span className="nav-end">
+                            <button onClick={() => handleSearch()}>
+                                <Search />
+                            </button>
+        
+                            <button onClick={() => showNotifications()}>
+                                <Notification />
+                            </button>
+                        </span>
+                    </nav>
+                </div>
+            );
+    
+        case '/academy': 
+            return (
+                <div>
+                    <nav>
+                        <span className="nav-start">
+                            <img src={ColorLogo} alt="LingoThyme logo" height="50px"/>
+                        </span>
+                        <span></span>
+                        <span className="nav-center">
+                            <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                <p><div className="other-tab"> Hall</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/schedule" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Schedule</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/academy" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="current-tab"> Academy</div></p>
+                            </Link>
+                            <div className="break"></div>
+                            <Link to="/profile" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Profile</div></p>
+                            </Link>
+                        </span>
+        
+                        <span className="nav-end">
+                            <button onClick={() => handleSearch()}>
+                                <Search />
+                            </button>
+        
+                            <button onClick={() => showNotifications()}>
+                                <Notification />
+                            </button>
+                        </span>
+                    </nav>
+                </div>
+            );
+            case '/profile': 
+                return (
+                    <div>
+                        <nav>
+                            <span className="nav-start">
+                                <img src={ColorLogo} alt="LingoThyme logo" height="50px"/>
+                            </span>
+                            <span></span>
+                            <span className="nav-center">
+                                <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                    <p><div className="other-tab"> Hall</div></p>
+                                </Link>
+                                <div className="break"></div>
+                                <Link to="/schedule" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                        <p><div className="other-tab"> Schedule</div></p>
+                                </Link>
+                                <div className="break"></div>
+                                <Link to="/academy" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                        <p><div className="other-tab"> Academy</div></p>
+                                </Link>
+                                <div className="break"></div>
+                                <Link to="/profile" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
+                                        <p><div className="current-tab"> Profile</div></p>
+                                </Link>
+                            </span>
+            
+                            <span className="nav-end">
+                                <button onClick={() => handleSearch()}>
+                                    <Search />
+                                </button>
+            
+                                <button onClick={() => showNotifications()}>
+                                    <Notification />
+                                </button>
+                            </span>
+                        </nav>
+                    </div>
+                );
+    }
 }
     
 export default Navbar;
