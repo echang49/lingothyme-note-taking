cc1436940f670f99e21ee2e7b66c2c3ec944abab
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-06-17 05:00:44 -0400

    Profile page updated

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -2,8 +2,8 @@ import { useEffect, useState, useRef } from "react";
 import { Redirect, useLocation, Link} from "react-router-dom"; 
 
 import ColorLogo from "../assets/main-logo.png";
+import ProfilePic from "../assets/users/Profile1.webp";
 
-import User from "./viewComponents/users";
 import {ReactComponent as Logo} from "../assets/main-logo.svg";
 import {ReactComponent as Search} from "../assets/search-icon.svg"; // edit svg properties, change to camel case 
 import {ReactComponent as Notification} from "../assets/notification-icon.svg"; // edit svg properties, change to camel case 
@@ -22,11 +22,33 @@ function Profile() {
                     bool ?
                         <div className="MainHall">
                             <Navbar />
-                            <div className="body">
-                                <div className="canvas">
-                                    <div>
-                                        <h>Profile page WIP</h> <div className="break"></div>
+                            <div className="profile">
+                                <div className="profile-left-container">
+
+                                </div>
+                                <div className="profile-container">
+                                    <div className="remind">
+                                        <h>Remind</h>
                                     </div>
+
+                                    <div className="achievments">
+                                        <h>Achievements</h>
+                                    </div>
+
+                                    <div className="friends">
+                                        <h>Friends</h>
+                                    </div>
+                                    
+
+                                    <div className="profile-box">
+                                        <img src={ProfilePic} height="75px" width="75px" alt="User profile pic" />
+                                        <h>UserName</h>
+                                    </div>
+
+                                    <div className="about-me">
+                                        <h>About Me</h>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
