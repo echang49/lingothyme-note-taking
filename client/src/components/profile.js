02457b09d41c6274d472a6b7f613733eb2de91c5
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-06-18 04:31:24 -0400

    Profile page updated

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,13 +1,10 @@
-import { useEffect, useState, useRef } from "react";
-import { Redirect, useLocation, Link} from "react-router-dom"; 
-
-import ColorLogo from "../assets/main-logo.png";
-import ProfilePic from "../assets/users/Profile1.webp";
-
-import {ReactComponent as Logo} from "../assets/main-logo.svg";
-import {ReactComponent as Search} from "../assets/search-icon.svg"; // edit svg properties, change to camel case 
-import {ReactComponent as Notification} from "../assets/notification-icon.svg"; // edit svg properties, change to camel case 
+import { useState } from "react";
+import { Redirect } from "react-router-dom"; 
 import firebase from "../firebase.js";
+
+import ProfilePic from "../assets/users/Profile1.webp";
+import {ReactComponent as Edit} from "../assets/edit-icon.svg"; // taken from https://iconmonstr.com/edit-9-svg/, replace with custom icon later
+
 import Navbar from "./viewComponents/navbar";
 
 
@@ -15,6 +12,10 @@ function Profile() {
     const [bool, setBool] = useState(true);
     const [loggedIn, setLoggedIn] = useState(false);
     const [phase, setPhase] = useState(1);
+
+    function handleEditClick(){
+        
+    }
 
         return(
             <div>
@@ -41,12 +42,25 @@ function Profile() {
                                     
 
                                     <div className="profile-box">
-                                        <img src={ProfilePic} height="75px" width="75px" alt="User profile pic" />
-                                        <h>UserName</h>
+                                        <div className="profile-box-inner"> 
+                                            <div className="profile-pic">
+                                                <img src={ProfilePic} height="75px" width="75px" alt="User profile pic" />
+                                            </div>
+                                            <Edit className="edit-icon" onclick={()=> handleEditClick}/>
+                                            <div className="username"><h>UserName</h></div>
+                                            
+                                        </div>
+                                        
                                     </div>
 
                                     <div className="about-me">
-                                        <h>About Me</h>
+                                        <div className="about-me-inner">
+                                            <Edit className="edit-icon" onclick={()=> handleEditClick}/>
+                                            <div className="title"><h>About Me</h></div>
+                                            <diV className="content"><p>
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque aliquam vestibulum morbi blandit cursus. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Varius vel pharetra vel turpis nunc eget lorem dolor. Suscipit tellus mauris a diam maecenas sed enim.
+                                                </p></diV>
+                                        </div>
                                     </div>
 
                                 </div>
