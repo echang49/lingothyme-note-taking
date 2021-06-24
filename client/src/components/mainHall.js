436d24c6697b263d14fe3ab83affcc54b560fb48
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-06-24 05:16:57 -0400

    mainHall updated

--- client/src/components/mainHall.js
+++ client/src/components/mainHall.js
@@ -5,18 +5,26 @@ import ColorLogo from "../assets/main-logo.png";
 import OngoingCard from "./viewComponents/ongoingCard"; 
 import ScheduledCard from "./viewComponents/scheduledCard";
 import Navbar from "./viewComponents/navbar"; 
+import Logo from "../assets/main-logo.png";
 
 import firebase from "../firebase.js";
+import axios from "axios";
 
 
 function MainHall() {
     const nameInput = useRef(null);
     const [bool, setBool] = useState(true);
     const [user, setUser] = useState({ loggedIn: false });
+    const userInput = useRef(null);
     const emailInput = useRef(null);
     const passInput = useRef(null);
     const [phase, setPhase] = useState(1);
     const auth = firebase.auth();
+
+    const numberInput = useRef(null);
+    const numberValue = useRef(null);
+    const questionInput = useRef(null);
+    const dateInput = useRef(null);
 
     async function handleLogin() { 
         let email = emailInput.current.value;
@@ -48,7 +56,6 @@ function MainHall() {
         return new Promise((resolve) => {
             setTimeout(resolve, ms);
         });
-        
     }
 
     function onAuthStateChange(callback) {
@@ -67,6 +74,56 @@ function MainHall() {
           unsubscribe();
         };
       }, []);
+
+      function changeNumber() {
+        let number = numberInput.current.value;
+        numberValue.current.innerHTML = number;
+      }
+    
+      function changeDate() {
+        let date = dateInput.current.valueAsDate;
+        let d = new Date();
+        d.setMonth(d.getMonth() + 1);
+        if(date > d) {
+          alert("We only support creating rooms to 1 month from now.");
+          dateInput.current.valueAsDate = new Date();
+        }
+      }
+
+    function createRoom(){
+        if(questionInput.current.value !== "") {
+            if(questionInput.current.value.length <= 240) {
+              let date = new Date(dateInput.current.value);
+              date.setDate(date.getDate() + 1); //add 1 day to the date of discussion for leeway
+              let data = {
+                email: emailInput.current.value,
+                number: numberInput.current.value,
+                question: questionInput.current.value,
+                date: date
+              }
+              axios.post('/api/auth/createRoom', data)
+                .then((res) => {
+                  let { publicKey, privateKey } = res.data;
+                  alert("Your public key is: " + publicKey + ". \nYour private key is: " + privateKey + ". \n\nYour public key is for the participants of the room and the private key is for you." +
+                  " To use the private key on the home page, enter the code as \"" + publicKey + "-" + privateKey + "\". \n\n NOTE: YOUR ROOM EXPIRES " + (date.getMonth() + 1) + "-" + (date.getDate() + 1) + "-" + date.getFullYear() + " AT MIDNIGHT EST/EDT.");
+                })
+                .catch((err) => {
+                  alert(err);
+              })
+              setPhase(1);
+            }
+            else {
+              alert("The maximum length allowed for questions is 240 characters.");
+            }
+          }
+          else {
+            alert("Please enter a question.");
+          }
+    }
+
+    function joinRoom() {
+        setPhase(1);
+      }
 
     //firebase WIP 3
 
@@ -103,43 +160,100 @@ function MainHall() {
             </div>
         );
     }
-    // else {
-        switch(phase) {
-            case 1:
-                return(
-                    <div>
-                        {
-                            bool ?
-                                <div className="MainHall">
-                                    <Navbar />
-                                    <div className="body">
-                                        <div className="canvas">
-                                            <div>
-                                                <h>main hall</h> <div className="break"></div>
-                                                <h>Ongoing</h> <div className="break"></div> 
-                                                <OngoingCard />
-                                                <h>Scheduled</h> <div className="break"></div>
-                                                <ScheduledCard />
 
-                                                <Link to="/mainHall" style={{ textDecoration: 'none' }}>  {/* remove link styling */}
-                                                    <button onClick={() => handleLogout()}>
-                                                        <p>Logout</p>
-                                                    </button>
-                                                </Link>
+    switch(phase) {
+        case 1: // main hall
+            return(
+                <div>
+                    {
+                        bool ?
+                            <div className="MainHall">
+                                <Navbar />
+                                <div className="body">
+                                    <div className="canvas">
+                                        <div classname="canvas-inner">
+                                            <h1>Ongoing</h1> <div className="break"></div> 
+                                            <OngoingCard />
+                                            <h2>Scheduled</h2> <div className="break"></div>
+                                            <ScheduledCard />
+                                            
+                                            <button onClick={() => setPhase(2)}>
+                                                <p>Create Room</p>
+                                            </button>
+                                            
+                                            <button onClick={() => setPhase(3)}>
+                                                <p>Join Room</p>
+                                            </button>
 
-                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
-                            :
-                                <Redirect to="/" />
-                        }
-                    </div>
-                );
-        }
-    //}
+                            </div>
+                        :
+                            <Redirect to="/" />
+                    }
+                </div>
+            );
+        case 2: // room creation
+            return ( // TODO: store user's created rooms in db for mainhall render
+                <div>
+                    {
+                        bool ?
+                            <div className="createRoom">
+                                <img src={Logo} alt="LingoThyme Logo" height="100px"/>
+                                <div className="flex-column">
+                                    <div className="input">
+                                        <label>Participant Number:</label>
+                                        <div className="range">
+                                            <input type="range" min="2" max="8" step="1" ref={numberInput} onChange={() => changeNumber()}/>
+                                            <span class="range-value" ref={numberValue}>8</span>
+                                        </div>
+                                    </div>
 
-    //
+                                    <div className="input">
+                                        <label>Question Field:</label>
+                                        <textarea rows="5" type="text" ref={questionInput} />
+                                    </div>
+
+                                    <div className="input">
+                                        <label>Flow of Events:</label>
+                                        <p className="flow">Changing flow of events is currently not supported. The current flow is 'brainstorming', 'paragraph writing', and then 'end'.</p>
+                                    </div>
+
+                                    <div className="input">
+                                        <label>Date of discussion</label>
+                                        <input type="date" ref={dateInput} onChange={() => changeDate()}/>
+                                    </div>
+
+                                    <div className="buttons">
+                                        <button className="primary-button" onClick={() => createRoom()}>CREATE</button>
+                                        <button className="secondary-button" onClick={() => setPhase(1)}>CANCEL</button>
+                                    </div>
+                                </div>
+                            </div>
+                        :
+                        <Redirect to="/" />
+                    }
+                </div>
+            
+            );
+        case 3: // join room
+            return( // TODO: store user's joined rooms in db for mainhall render
+                <div>
+                    <div className="enterRoom center">
+                        <img src={Logo} alt="LingoThyme logo" height="250px"/>
+                        <div className="input">
+                            <label>Room Code:</label>
+                            <input type="text" ref={nameInput} />
+                            <div className="buttons">
+                                <button className="primary-button" onClick={() => joinRoom()}>ENTER</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+    }
 }
     
 export default MainHall;
