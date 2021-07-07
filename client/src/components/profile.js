33295fb76e89ae5cf8281c8b0685926b866a80a8
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-07-06 21:48:27 -0400

    fixed profile render when not logged in

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -1,8 +1,9 @@
-import { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom"; 
+import { useEffect, useState, useRef } from "react";
+import { Redirect, Link} from "react-router-dom"; 
 import firebase from "../firebase.js";
 import axios from "axios";
 
+import ColorLogo from "../assets/main-logo.png";
 import ProfilePic from "../assets/users/Profile1.webp";
 import {ReactComponent as Edit} from "../assets/edit-icon.svg"; // taken from https://iconmonstr.com/edit-9-svg/, replace with custom icon later
 
@@ -11,24 +12,21 @@ import Navbar from "./viewComponents/navbar";
 
 function Profile() {
     const [bool, setBool] = useState(true);
+    const emailInput = useRef(null);
+    const passInput = useRef(null);
     //const [loggedIn, setLoggedIn] = useState(false);
     //const [phase, setPhase] = useState(1);
     const [user, setUser] = useState({ loggedIn: false });
-    const [email, setEmail] = useState('');
     const [aboutMeText, setAboutMeText] = useState("");
-    const [currentUser, setCurrentUser] = useState(undefined);
     const auth = firebase.auth();
     //setBool(loggedIn); // if logged in, render profile page, else redirect to /mainHall
 
     function onAuthStateChange(callback) {
         return firebase.auth().onAuthStateChanged(async user => {
             if (user) {
-                console.log(user.email)
-                callback({loggedIn: true});
-                let userEmail = user.email;
-                setEmail(userEmail);
-                const res = await axios.post('/api/auth/getAboutMeText', {email: userEmail})
-                setAboutMeText(res.data);
+                callback({loggedIn: true}); // set user login state to true
+                const res = await axios.post('/api/auth/getAboutMeText', {email: user.email})
+                setAboutMeText(res.data);  
                 console.log(aboutMeText);
             } else {
                 callback({loggedIn: false});
@@ -43,16 +41,53 @@ function Profile() {
         };
     }, []);
 
+    async function handleLogin() { 
+        let email = emailInput.current.value;
+        let pass = passInput.current.value;
+        auth.signInWithEmailAndPassword(email, pass)
+        .then(() => {
+            setUser(auth.currentUser); 
+            <Redirect to="/mainHall" />
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+                alert('Wrong password.');
+            } else {
+                alert('Error: ' + errorMessage);
+            }
+            console.log(error);
+        });     
+    }
 
     function handleEditClick(){
         
     }
     
-    // TODO: make sure user cannot access profile page when not logged in
-    //if(loggedIn){
-    
-    //var aboutMeText = axios.post('/api/auth/getAboutMeText', auth.currentUser.email);
-    //console.log(aboutMeText);
+    if(!user.loggedIn) { // user not logged in, prompt them to login or signup
+        return(
+            <div className="enterRoom center">
+                <img src={ColorLogo} alt="LingoThyme Logo" height="250px"/>
+                <div className="input">
+                    <label>Email:</label>
+                    <input type="text" ref={emailInput} />
+
+                    <label>Password:</label>
+                    <input type="password" ref={passInput} />
+                    
+                    <Link to="/signup" style={{ textDecoration: 'none' }}>Sign up</Link>
+                    <Link to="/passwordReset" style={{ textDecoration: 'none' }}>Forgot password?</Link>
+                    <div className="buttons">
+                        <button className="primary-button" onClick={() => handleLogin()} >LOGIN</button> 
+                        <Link className="secondary-button" to="/">RETURN</Link>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
     return(
         <div>
             {
