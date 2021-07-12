78030dd27297c5b70fd02ee727beedf8765a7aff
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-07-12 17:31:12 -0400

    Update scheduledCard.js

--- client/src/components/viewComponents/scheduledCard.js
+++ client/src/components/viewComponents/scheduledCard.js
@@ -1,16 +1,15 @@
 import Calendar from "../../assets/calendar-icon.svg";
 import People from "../../assets/people-icon.svg";
-function scheduledCard(props) { // same as ongoingCard, with added number of current users in room
+function scheduledCard({title, card_content, time, capacity }) { // same as ongoingCard, with added number of current users in room
     return (
         <div className="card">
             <div className="card-header"> 
                 {/* add icon here  */}
-                <h1>Idea Brainstorming</h1>
+                <h1>{title}</h1>
             </div>
             <hr></hr>
             <div className="card-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada massa odio, sed 
-                    sagittis justo vehicula eget. In pharetra auctor purus, et pretium urna dictum quis.</p>
+                <p>{card_content}</p>
             </div>
 
             <div className="icon1">
@@ -18,15 +17,16 @@ function scheduledCard(props) { // same as ongoingCard, with added number of cur
             </div>
 
             <div className="date-text">
-                <p><div className="time"> 06 July 2021 10:00</div></p>
+                <p><div className="time"> {time}</div></p>
             </div>
 
             <div className="icon2-scheduled">
                 <img src={People} alt="add people icon" height="30px"/>
             </div>
 
+
             <div className="room-size-text-scheduled">
-                <p>6/8</p>
+                <p>{capacity}</p>
             </div>
 
             <button className="join-button-scheduled">Join</button>
