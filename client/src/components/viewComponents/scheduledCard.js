dba8413e3b55d03ae641a1e6c108b5c61b26c5de
Vimal Makenthirarasa <vimal.maken@gmail.com>
2021-07-13 18:34:25 -0400

    moved joinRoom to cards, WIP

--- client/src/components/viewComponents/scheduledCard.js
+++ client/src/components/viewComponents/scheduledCard.js
@@ -1,6 +1,45 @@
+import { useState, useEffect, useRef } from "react";
+import { Redirect, useLocation, Link} from "react-router-dom"; 
+import axios from 'axios';
+
 import Calendar from "../../assets/calendar-icon.svg";
 import People from "../../assets/people-icon.svg";
-function scheduledCard({title, card_content, time, capacity }) { // same as ongoingCard, with added number of current users in room
+
+
+function ScheduledCard({roomKey, title, card_content, time, capacity }) { // same as ongoingCard, with added number of current users in room
+    const textInput = useRef(null);
+    const [ url, setURL ] = useState("/");
+    const [bool, setBool] = useState(true);
+    const [phase, setPhase] = useState(1);
+
+    // useEffect(() => {
+        
+    //   }, []);
+
+    function joinRoom() { // join room
+        let code = roomKey;
+        console.log("joining room with code: " + code);
+        axios.post("/api/auth/mainhall_joinRoom", {code})
+        .then((res) => {
+            if(res.data[0]) {
+                if(res.data[1]) {
+                    setURL("/room?id=".concat(code));
+                    console.log("sending to room...");
+                    <Redirect push to={url} />
+                }
+                else {
+                    
+                }
+                setBool(false);
+            }
+            else {
+                alert("Incorrect code. Ensure you have the proper room code.");
+            }
+        })
+        .catch((err) => {
+            alert(err);
+        });  
+    }
     return (
         <div className="card">
             <div className="card-header"> 
@@ -29,11 +68,11 @@ function scheduledCard({title, card_content, time, capacity }) { // same as ongo
                 <p>{capacity}</p>
             </div>
 
-            <button className="join-button-scheduled">Join</button>
+            <button className="join-button-scheduled" onClick={() => joinRoom()}>Join</button>
         </div>
         
         
     );
 }
     
-export default scheduledCard;
+export default ScheduledCard;
